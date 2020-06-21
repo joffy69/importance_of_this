399fb20ef6381d7e99fde209aef5260356225368
joffy69 <jcwgibbs@gmail.com>
2020-06-21 16:27:14 +0000

    finished all examples

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-$('p').click(function(){
-    $('p').slideToggle('slow');
+$(document).ready(function(){
+    $('.box').on('click',function(){
+        var classNames=$(this).attr("class").split(" ");
+        $("."+classNames[1]).css("background-color", "red");
+    });
 });
+
+$('p').click(function(){
+    $(this).slideToggle('slow');
+});
+
+$('.btn').mouseenter(function(){
+    $(this).removeClass('makeRed').addClass('makeBlue');
+})
+$('.btn').mouseleave(function(){
+    $(this).removeClass('makeBlue').addClass('makeRed');
+})
+
